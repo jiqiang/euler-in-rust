238a5eab487e1134f830a01ba4d9cd2375d2aea3
Qiang Ji <qiang.j@gmail.com>
2019-01-14 21:46:01 +1100

    create p001 solution

--- src/bin/p001.rs
+++ src/bin/p001.rs
@@ -1,3 +1,25 @@
+extern crate threadpool;
+
+use std::sync::mpsc::channel;
+use threadpool::ThreadPool;
+
 fn main() {
-    println!("Hello, world!");
+    let num_workers = 4;
+    let num_jobs = 1000;
+    let pool = ThreadPool::new(num_workers);
+    let (tx, rx) = channel();
+    for n in 0..num_jobs {
+        let tx = tx.clone();
+        pool.execute(move || {
+            let num = if (n >= 3 && n % 3 == 0) || (n >= 5 && n % 5 == 0) {
+                n
+            } else {
+                0
+            };
+            tx.send(num)
+                .expect("channel will be there waiting for the pool");
+        });
+    }
+    let sum: usize = rx.iter().take(num_jobs).sum();
+    println!("{}", sum);
 }
