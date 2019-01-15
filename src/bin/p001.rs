24690d8d17018d20bdc898405be19fde759da978
Qiang Ji <qiang.j@gmail.com>
2019-01-15 20:27:06 +1100

    refactor variable names

--- src/bin/p001.rs
+++ src/bin/p001.rs
@@ -4,22 +4,22 @@ use std::sync::mpsc::channel;
 use threadpool::ThreadPool;
 
 fn main() {
-    let num_workers = 4;
-    let num_jobs = 1000;
-    let pool = ThreadPool::new(num_workers);
-    let (tx, rx) = channel();
-    for n in 0..num_jobs {
-        let tx = tx.clone();
+    let num_of_workers = 4;
+    let max_num = 1000;
+    let pool = ThreadPool::new(num_of_workers);
+    let (sender, receiver) = channel();
+    for n in 0..max_num {
+        let tx = sender.clone();
         pool.execute(move || {
-            let num = if (n >= 3 && n % 3 == 0) || (n >= 5 && n % 5 == 0) {
+            let value = if (n >= 3 && n % 3 == 0) || (n >= 5 && n % 5 == 0) {
                 n
             } else {
                 0
             };
-            tx.send(num)
+            tx.send(value)
                 .expect("channel will be there waiting for the pool");
         });
     }
-    let sum: usize = rx.iter().take(num_jobs).sum();
+    let sum: usize = receiver.iter().take(max_num).sum();
     println!("{}", sum);
 }
