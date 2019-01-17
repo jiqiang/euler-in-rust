a8515609c8d76b4f5f2644908a1bc64390fad50b
Qiang Ji <qiang.j@gmail.com>
2019-01-17 21:37:36 +1100

    fix cargo fmt

--- benches/benchmarks/p001.rs
+++ benches/benchmarks/p001.rs
@@ -6,5 +6,7 @@ pub fn bench_p001_concurrently(c: &mut Criterion) {
 }
 
 pub fn bench_p001_not_concurrently(c: &mut Criterion) {
-    c.bench_function("p001_not_concurrently", |b| b.iter(|| s001::run(3, 5, 1000)));
+    c.bench_function("p001_not_concurrently", |b| {
+        b.iter(|| s001::run(3, 5, 1000))
+    });
 }
--- benches/euler_benchmark.rs
+++ benches/euler_benchmark.rs
@@ -3,7 +3,11 @@ extern crate criterion;
 
 mod benchmarks;
 
-criterion_group!(p001, benchmarks::p001::bench_p001_concurrently, benchmarks::p001::bench_p001_not_concurrently);
+criterion_group!(
+    p001,
+    benchmarks::p001::bench_p001_concurrently,
+    benchmarks::p001::bench_p001_not_concurrently
+);
 criterion_main! {
     p001,
 }
