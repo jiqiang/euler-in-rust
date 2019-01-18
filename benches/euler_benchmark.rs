4e7220fc5325bba25e9aaa09c51ade74d339745a
Qiang Ji <qiang.j@gmail.com>
2019-01-18 21:59:34 +1100

    fix cargo fmt

--- benches/euler_benchmark.rs
+++ benches/euler_benchmark.rs
@@ -9,10 +9,7 @@ criterion_group!(
     benchmarks::p001::bench_p001_not_concurrently
 );
 
-criterion_group!(
-    p002,
-    benchmarks::p002::bench_p002
-);
+criterion_group!(p002, benchmarks::p002::bench_p002);
 
 criterion_main! {
     p001,
