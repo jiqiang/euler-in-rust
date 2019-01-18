feb6b355f9e5574c037393ad2aed88deef282bf7
Qiang Ji <qiang.j@gmail.com>
2019-01-18 22:11:28 +1100

    fix cargo fmt

--- benches/benchmarks/p001.rs
+++ benches/benchmarks/p001.rs
@@ -2,7 +2,9 @@ use criterion::Criterion;
 use euler::solution::s001;
 
 pub fn bench_p001_1(c: &mut Criterion) {
-    c.bench_function("p001_1", |b| b.iter(|| s001::multiples_of_3_and_5_1(3, 5, 1000, 4)));
+    c.bench_function("p001_1", |b| {
+        b.iter(|| s001::multiples_of_3_and_5_1(3, 5, 1000, 4))
+    });
 }
 
 pub fn bench_p001_2(c: &mut Criterion) {
