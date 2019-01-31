8960e38b9740102c2722172fd6c985771ec938b7
Glenn Ji <glenn.ji@nab.com.au>
2019-01-31 15:33:48 +1100

    add solution for p005

--- src/solution/s005.rs
+++ src/solution/s005.rs
@@ -1,21 +1,23 @@
-pub fn is_prime(num: u32) -> bool {
-    num != 0 && (2..num).find(|n| num % n == 0) == None
-}
-
-pub fn get_prime_factors(mut num: u32) -> Vec<u32> {
-    let mut lcm: Vec<u32> = Vec::new();
-    let mut n = 2;
+pub fn lcm(max: usize) -> usize {
+    let mut is_lcm: bool;
+    let mut lcm: usize = max;
+    let mut i: usize = 2;
     loop {
-        if n > num {
+        is_lcm = true;
+
+        for n in 2..max {
+            if lcm % n != 0 {
+                is_lcm = false;
+                break;
+            }
+        }
+
+        if is_lcm {
             break;
         }
 
-        if is_prime(n) && num % n == 0 {
-            lcm.push(n);
-            num /= n;
-        } else {
-            n += 1;
-        }
+        lcm = max * i;
+        i += 1;
     }
     lcm
 }
@@ -25,32 +27,8 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_is_prime() {
-        let tests = [
-            (false, 0),
-            (true, 1),
-            (true, 2),
-            (true, 3),
-            (false, 4),
-        ];
-        for test in tests.iter() {
-            assert_eq!(test.0, is_prime(test.1));
-        }
-    }
-
-    #[test]
-    fn test_get_prime_factors() {
-        let tests = [
-            (vec![1], 1),
-            (vec![2], 2),
-            (vec![3], 3),
-            (vec![2, 2], 4),
-            (vec![5], 5),
-            (vec![2, 3], 6),
-            (vec![2, 2, 5, 5], 100),
-        ];
-        for test in tests.iter() {
-            assert_eq!(test.0, get_prime_factors(test.1));
-        }
+    fn test_lcm() {
+        assert_eq!(2520, lcm(10));
+        assert_eq!(232792560, lcm(20));
     }
 }
