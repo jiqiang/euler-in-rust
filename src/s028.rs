be587f5c53c83da21b4e13c9585bd2260ad7c709
Glenn Ji <glenn.ji@nab.com.au>
2019-07-10 13:31:12 +1000

    add problem 28 solution skeleton

---
+++ src/s028.rs
@@ -0,0 +1,13 @@
+pub fn number_spiral_diagonals(_size: u32) -> u32 {
+    101
+}
+
+#[cfg(test)]
+mod s028_tests {
+    use super::*;
+
+    #[test]
+    fn test_number_spiral_diagonals() {
+        assert_eq!(101, number_spiral_diagonals(5))
+    }
+}
