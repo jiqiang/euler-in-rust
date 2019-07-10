c298873d7c9578f71dc6108bde62f9f842af53ee
Glenn Ji <glenn.ji@nab.com.au>
2019-07-10 15:13:34 +1000

    add problem 5 description

--- src/s005.rs
+++ src/s005.rs
@@ -1,3 +1,9 @@
+//! Problem 5: Smallest multiple
+//!
+//! 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+//!
+//! What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+
 pub fn smallest_multiple(max: usize) -> usize {
     let mut is_lcm: bool;
     let mut lcm: usize = max;
