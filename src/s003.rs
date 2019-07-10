495dc0caf07643ac1d7fba39b9de06b233c7ffc1
Glenn Ji <glenn.ji@nab.com.au>
2019-07-10 15:09:54 +1000

    add problem 3 description

--- src/s003.rs
+++ src/s003.rs
@@ -1,3 +1,9 @@
+//! Problem 3: Largest prime factor
+//!
+//! The prime factors of 13195 are 5, 7, 13 and 29.
+//!
+//! What is the largest prime factor of the number 600851475143?
+
 pub fn largest_prime_factor(mut num: usize) -> usize {
     let mut index = 2;
     while index < num {
