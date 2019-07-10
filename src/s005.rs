16434823126ba2fdb9c8465eda6830c4159e089c
Glenn Ji <glenn.ji@nab.com.au>
2019-07-10 14:13:22 +1000

    refactor s005

--- src/s005.rs
+++ src/s005.rs
@@ -1,4 +1,4 @@
-pub fn lcm(max: usize) -> usize {
+pub fn smallest_multiple(max: usize) -> usize {
     let mut is_lcm: bool;
     let mut lcm: usize = max;
     let mut i: usize = 2;
@@ -23,12 +23,12 @@ pub fn lcm(max: usize) -> usize {
 }
 
 #[cfg(test)]
-mod tests {
+mod s005_tests {
     use super::*;
 
     #[test]
-    fn test_lcm() {
-        assert_eq!(2520, lcm(10));
-        assert_eq!(232_792_560, lcm(20));
+    fn test_smallest_multiple() {
+        assert_eq!(2520, smallest_multiple(10));
+        assert_eq!(232_792_560, smallest_multiple(20));
     }
 }
